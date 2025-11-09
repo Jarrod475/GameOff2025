d2c67b00eb5ac1394b2034c0377952fbb479b0a6
jarry-linux <jarrodlondtwork@gmail.com>
2025-11-09 14:20:46 +0100

    poop

--- scripts/door_controller.gd
+++ scripts/door_controller.gd
@@ -5,24 +5,39 @@ var locked = false
 @onready var anim = $anim
 var is_open = false
 
+@export var light_green : Material
+@export var light_yellow : Material
+@export var light_red : Material
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player") and !locked and !anim.is_playing() and !is_open:
 		anim.play("open")
+		light.material_override = light_yellow
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body.is_in_group("player") and !anim.is_playing() and is_open:
 		anim.play_backwards("open")
+		light.material_override = light_yellow
 	else:
 		await  get_tree().create_timer(5).timeout
 		if is_open:
 			anim.play_backwards("open")
+			light.material_override = light_yellow
 
 
 func _on_anim_animation_finished(anim_name: StringName) -> void:
 	is_open = !is_open
+	if locked:
+		light.material_override = light_red
+	else:
+		light.material_override = light_green
 
 
-##contine here! light needs to change based on state unlocked, moving, locked.
 func lock():
 	locked = true
+	light.material_override = light_red
+
+func unlock():
+	locked = false
+	light.material_override = light_green
--- scripts/level_1_manager.gd
+++ scripts/level_1_manager.gd
@@ -1,1 +1,17 @@
 extends CanvasLayer
+
+@onready var spawn_point_parent = $ColorRect/SubViewport/Environment/enemy_spawns
+@onready var env = $ColorRect/SubViewport/Environment
+@onready var enemy = preload("res://scenes/enemy_2.tscn")
+
+func _ready() -> void:
+	return
+	await get_tree().create_timer(1).timeout
+	for n in spawn_point_parent.get_children():
+		for d in 5:
+			var new_enemy = enemy.instantiate()
+			env.add_child(new_enemy)
+			new_enemy.position = n.position
+
+##Jarryboi!
+##continue here. get this spawner to spawn them 
