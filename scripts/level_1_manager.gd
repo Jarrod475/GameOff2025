ce1ba9e522bcba1bb189a602a32f917f7ca10857
jarry-linux <jarrodlondtwork@gmail.com>
2025-11-13 21:49:11 +0100

    poop

--- scripts/level_1_manager.gd
+++ scripts/level_1_manager.gd
@@ -4,14 +4,37 @@ extends CanvasLayer
 @onready var env = $ColorRect/SubViewport/Environment
 @onready var enemy = preload("res://scenes/enemy_2.tscn")
 
+@onready var shader_controller = $ColorRect
+@onready var text_controller = $ColorRect/SubViewport/HUD/text_display_bottom
+
 @onready var spawn_timer = $spawn_timer
 var spawn_pipe_index = 1
 
-func _ready() -> void:
-	await get_tree().create_timer(1).timeout
-	spawn_timer.start()
+var terminal_1_active = false
+var terminal_2_active = false
 
+var enemy_list = []
+var enemy_limit = 20
+
+@export var debug = false
+
+func _ready() -> void:
+	$ColorRect/SubViewport/Environment/terminal.terminal_activated.connect(terminal_activated)
+	$ColorRect/SubViewport/Environment/terminal2.terminal_activated.connect(terminal_activated)
+	if debug:
+		spawn_timer.start()
+		return
+	
+	await get_tree().create_timer(1).timeout
+	text_controller.display_text("Wow a lockdown? Never had one of those...")
+	await get_tree().create_timer(5).timeout
+	text_controller.display_text("One of those clankers Must've escaped.")
+	await get_tree().create_timer(5).timeout
+	text_controller.display_text("Guess this is a job for ole Jimmy!")
+	
 func _on_spawn_timer_timeout() -> void:
+	if enemy_list.size() > enemy_limit:
+		spawn_timer.stop()
 	var new_enemy = enemy.instantiate()
 	spawn_pipe_index += 1
 	if spawn_pipe_index > 3:
@@ -21,3 +44,11 @@ func _on_spawn_timer_timeout() -> void:
 		2: new_enemy.position = $ColorRect/SubViewport/Environment/enemy_spawns/spawn_point_3.global_position
 		3: new_enemy.position = $ColorRect/SubViewport/Environment/enemy_spawns/spawn_point_4.global_position
 	env.add_child(new_enemy)
+	enemy_list.append(new_enemy)
+
+func terminal_activated():
+	if terminal_1_active:
+		terminal_2_active = true
+		spawn_timer.start()
+	else:
+		terminal_1_active = true
--- scripts/shader_controller.gd
+++ scripts/shader_controller.gd
@@ -8,14 +8,14 @@ var scanline_thickness = 0.75:
 	get(): return scanline_thickness
 	set(val):
 		scanline_thickness = val
-		scanline_thickness = clampf(val,0.0,1.0)
+		scanline_thickness = clampf(val,0.25,1.0)
 		material.set_shader_parameter("sharpness",val)
 
 var scanline_brightness = 1.0: 
 	get(): return scanline_brightness
 	set(val):
 		scanline_brightness = val
-		scanline_brightness = clampf(val,0.25,1.0)
+		scanline_brightness = clampf(val,0.0,1.0)
 		material.set_shader_parameter("scanline_brightness",val)
 
 func _ready() -> void:
@@ -41,6 +41,7 @@ func fade_to_black(time = 1.0):
 	enemy_is_close = false
 	var tween = get_tree().create_tween()
 	tween.tween_property(self,"scanline_brightness",0.0,time)
+
 
 
 #reset shader after charley is not near anymore
