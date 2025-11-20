287b28856cf890a021c2d3d7b6c97305b0cfe0d9
jarry-linux <jarrodlondtwork@gmail.com>
2025-11-20 11:51:56 +0100

    map finito lxl 2

--- scripts/door_controller.gd
+++ scripts/door_controller.gd
@@ -11,7 +11,11 @@ var is_open = false
 @export var light_yellow : Material
 @export var light_red : Material
 
+signal opening
+
+var locked_in_open_pos = false
 func _ready() -> void:
+	#toggle_stay_open()
 	if locked:
 		light.material_override = light_red
 	else:
@@ -20,13 +24,18 @@ func _ready() -> void:
 		
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
+	if locked_in_open_pos:
+		return
 	if body.is_in_group("player") and !locked and !anim.is_playing() and !is_open:
+		opening.emit()
 		anim.play("open")
 		audio.play()
 		light.material_override = light_yellow
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
+	if locked_in_open_pos:
+		return
 	if body.is_in_group("player") and !anim.is_playing() and is_open:
 		anim.play_backwards("open")
 		audio.play()
@@ -57,3 +66,11 @@ func unlock():
 
 func toggle_invisible_wall():
 	invisible_wall_coll.disabled = !invisible_wall_coll.disabled
+
+func toggle_stay_open():
+	locked_in_open_pos = !locked_in_open_pos
+	if locked_in_open_pos:
+		anim.play("open",-1,1,true)
+	else:
+		anim.play("RESET")
+		
